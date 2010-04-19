2a94f3da7c19c770423ec9f618fbaa08c474af1f
Kevin Cathcart <kcathcar@nd.edu>
2010-04-19 10:46:41 -0400

    Removed old Rails 3 Beta 2 initializer no longer needed in Rails 3 Beta 3

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.cookie_secret = '5ce2bc1021cb67da3cd7b103764cd1427c4c02564bf2f15834ccff068ffd656e357aae214c3598a26f81f2166285f3fed8e9a433c105140b03f7ace9b99c4672'
