921854dfbf9ea536e5ece1321d4619d46c501ed7
Kevin Cathcart <kcathcar@nd.edu>
2010-04-19 11:50:32 -0400

    'rpX signin should now work on the server in addition to development

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def rpx_url
+    'https://bug-tracker.rpxnow.com/openid/v2/signin?' +
+	{:token_url => (url_for :controller => 'sessions', :action => :create, :host => request.host)}.to_query
+  end
 end
